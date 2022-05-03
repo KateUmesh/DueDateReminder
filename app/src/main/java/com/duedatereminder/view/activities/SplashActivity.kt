f206822521973c7265b63605d982f69912f86ef8
KateUmesh <umeshskate2111@gmail.com>
2022-05-04 01:57:55 +0530

    Change parent theme in themes.xml

--- app/src/main/java/com/duedatereminder/view/activities/SplashActivity.kt
+++ app/src/main/java/com/duedatereminder/view/activities/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.duedatereminder.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.duedatereminder.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
