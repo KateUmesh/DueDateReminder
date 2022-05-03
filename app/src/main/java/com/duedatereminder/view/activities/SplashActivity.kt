0952cc6ed85ffd11d01469d804a8204e4bc64240
KateUmesh <umeshskate2111@gmail.com>
2022-05-04 00:14:09 +0530

    Initial commit

---
+++ app/src/main/java/com/duedatereminder/view/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.duedatereminder.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.duedatereminder.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
