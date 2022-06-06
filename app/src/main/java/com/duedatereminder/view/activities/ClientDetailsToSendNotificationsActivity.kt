12dbaa2ac0a4517a188476516f68a238b77b5b9e
KateUmesh <umeshskate2111@gmail.com>
2022-06-07 00:33:37 +0530

    ClientDetailsToSendNotificationsActivity.kt added

--- app/src/main/java/com/duedatereminder/view/activities/ClientDetailsToSendNotificationsActivity.kt
+++ app/src/main/java/com/duedatereminder/view/activities/ClientDetailsToSendNotificationsActivity.kt
@@ -2,12 +2,17 @@ package com.duedatereminder.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.duedatereminder.R
+import com.duedatereminder.callback.SnackBarCallback
+import com.duedatereminder.utils.ContextExtension.Companion.showSnackBar
+import com.duedatereminder.utils.ContextExtension.Companion.toolbar
+import com.duedatereminder.utils.NetworkConnection
 import com.duedatereminder.viewModel.activityViewModel.ViewModelNotificationTemplates
 
-class ClientDetailsToSendNotificationsActivity : AppCompatActivity() {
+class ClientDetailsToSendNotificationsActivity : AppCompatActivity(), SnackBarCallback {
     lateinit var rvClientDetails: RecyclerView
     private lateinit var mViewModelNotificationTemplates: ViewModelNotificationTemplates
     private lateinit var llLoading : LinearLayoutCompat
@@ -15,5 +20,25 @@ class ClientDetailsToSendNotificationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_details_to_send_notifications)
+
+        /**Toolbar*/
+        toolbar(getString(R.string.client_details),true)
+    }
+
+    private fun callGetNotificationTemplatesApi(){
+        llLoading.visibility = View.VISIBLE
+        if(NetworkConnection.isNetworkConnected()) {
+            mViewModelNotificationTemplates.getNotificationTemplates(idNotificationCategory)
+        }else{
+            showSnackBar(this,getString(R.string.no_internet_connection))
+        }
+    }
+
+    override fun snackBarSuccessInternetConnection() {
+        mViewModelNotificationTemplates.getNotificationTemplates(idNotificationCategory)
+    }
+
+    override fun snackBarFailedInterConnection() {
+        showSnackBar(this,getString(R.string.no_internet_connection))
     }
 }
